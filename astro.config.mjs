7ddf22c159a73b469ec50dbe898a3bd23e7e4f48
Matthew Bramich <matt_bramich@hotmail.com>
2024-03-21 10:51:01 +1100

    feat: create button component

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react(), icon()],
+	integrations: [tailwind(), react(), icon({
+		iconDir: "./src/assets/icons",
+	  })],
 });
