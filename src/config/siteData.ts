fd6b31788f025f9fa636a2b958ec003daf253bbe
matt <matt_bramich@hotmail.com>
2024-04-17 10:56:24 +1000

    chore: update og image

--- src/config/siteData.ts
+++ src/config/siteData.ts
@@ -48,7 +48,7 @@ const siteData: SiteDataProps = {
 
 	// default image for meta tags if the page doesn't have an image already
 	defaultImage: {
-		src: '/src/assets/images/gglogo.png',
+		src: '/gglogo.png',
 		alt: 'Gutter Gladiators Logo'
 	}
 };
