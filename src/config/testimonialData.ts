f891a5ef0b4bc9b1206e911b58fc425cf2e2e1b3
matt <matt_bramich@hotmail.com>
2024-05-09 10:13:50 +1000

    chore: add client content

--- src/config/testimonialData.ts
+++ src/config/testimonialData.ts
@@ -14,26 +14,21 @@ const testimonialData: TestimonialItem[] = [
 		avatar: girl1,
 		name: 'Caitlin',
 		title: 'Client',
-		testimonial: `It was great experience working with Cosmic Themes. 
-        They were very professional and responsive throughout the entire website design process.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+		testimonial: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
       `
 	},
 	{
 		avatar: BowTiedFocus,
 		name: 'BowTiedFocus',
 		title: 'Frontend Engineer',
-		testimonial: `The Blogsmith Pro theme is ridiculously well put together and documented.
-        I learned a ton about Astro engineering by studying it, and I've already used some of the components
-        for my web design clients. 
+		testimonial: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. 
         `
 	},
 	{
 		avatar: TravisB,
 		name: 'Travis B',
 		title: 'Developer',
-		testimonial: `Cosmic themes are for webdevs and marketers who don't want to waste time reinventing the wheel. 
-      Their themes have great examples of some of the creative things you can accomplish with Astro. 
+		testimonial: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. 
         `
 	}
 ];
