bc6773a7c14c5553eaafee0fea139ce479408f6b
matt <matt_bramich@hotmail.com>
2024-05-09 18:33:22 +1000

    fix: hero swiper not working
    This commit fixes the issue where the background image
    in the Hero for the swiper was collapsed. This was a css
    specificty issue. Had to declare the classes manually and
    not with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 // Autoprefix automatically included
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	// important: true,
 	theme: {
 		extend: {
 			colors: {
